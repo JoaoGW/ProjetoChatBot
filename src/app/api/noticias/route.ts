87565ea92104a4e58ece5f1b2f958fe3b4935090
João Ribeiro <cpsenha@gmail.com>
2024-09-26 18:08:51 -0300

    Finalizada com sucesso a requisicao de todas as informacoes relevates da API, com as seguintes configuracoes: quote: stock, language: pt, from September 1st, sortby: popularity. Key devidamente escondida o qual o repositorio nao tera acesso a informacao por se tratar de uma informacao unica e privada.

--- src/app/api/noticias/route.ts
+++ src/app/api/noticias/route.ts
@@ -1,25 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { NEWS_KEY } from '../../../../envLocal';
 
 export async function GET(request: NextRequest): Promise<Response> {
     try {
-        //Definicao dos valores de rota de fetch
-        const news_url = "https://newsapi.org/v2/everything?q=bitcoin&from=2024-09-01&sortBy=publishedAt&apiKey="
-        const key = process.env.NEWS_KEY
+        // Definicao dos valores de rota de fetch
+        const key = NEWS_KEY;
+
+        // Definicao da localizacao preferida das noticias a serem recebidas
         const country = request.nextUrl.searchParams.get('country') || 'br';
 
-        //Rota da api com a minha KEY pessoal
-        const response = await fetch(`${news_url}${key}`)
+        // Rota da api com a minha KEY pessoal, usando template string corretamente
+        const response = await fetch(`https://newsapi.org/v2/everything?q=stock&language=pt&from=2024-09-01&to=2024-09-26&sortBy=popularity&apiKey=${key}`);
 
-        //Verifica o resultado da response do fetch
-        if(!response.ok){
-            throw new Error("Ocorreu um erro no fetch das informacoes");    
+        // Verifica o resultado da response do fetch
+        if (!response.ok) {
+            throw new Error("Ocorreu um erro no fetch das informacoes");
         }
 
-        //Armazenamento do resultado da rota e retorno de valor com codigo de sucesso 200
+        // Armazenamento do resultado da rota e retorno de valor com codigo de sucesso 200
         const data = await response.json();
         return NextResponse.json({ data }, { status: 200 });
     } catch (error) {
-        //Retorno de erro para caso ocorra algum erro na requisicao, adivindo de !ok
+        // Retorno de erro para caso ocorra algum erro na requisicao, adivindo de !ok
         return NextResponse.json({ message: "Ocorreu um problema na requisição das noticias" }, { status: 500 });
     }
 }
