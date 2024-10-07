551d30bf1ba23f329f33a225b246009942209e1b
aharp44 <lucaskhayashi@gmail.com>
2024-10-07 16:43:22 -0300

    adicionado API de conversao de moeda, taxa de inflacao e juros

---
+++ src/app/api/juros/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest): Promise<Response> {
+    try {
+               // Rota da api com a minha KEY pessoal, usando template string corretamente
+        const response = await fetch(`https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados/ultimos/10?formato=json`);
+
+        // Verifica o resultado da response do fetch
+        if (!response.ok) {
+
+            throw new Error("Ocorreu um erro no fetch das informacoes");
+        }
+
+        // Armazenamento do resultado da rota e retorno de valor com codigo de sucesso 200
+        const data = await response.json();
+        return NextResponse.json({ data }, { status: 200 });
+    } catch (error) {
+        // Retorno de erro para caso ocorra algum erro na requisicao, adivindo de !ok
+        return NextResponse.json({ message: (error as Error).message }, { status: 500 });
+    
+    }
+}
